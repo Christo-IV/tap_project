928e248099f2452d5ad958bb7f4ecd88a284fabd
Sander <sander.paasalu@khk.ee>
2021-05-11 09:36:29 +0300

    register

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -13,10 +13,34 @@ class Users extends Controller
 
     public function register() {
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+            print_r($_POST['email']);
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = array(
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            );
+             if(empty($data['name'])){
+                 $data['name_err'] = 'Please enter the name';
+             }
+             if(empty($data['email'])){
+                 $data['email_err'] = 'Please enter the email';
+             }
+             if(empty($data['password'])){
+                 $data['password_err'] = 'Please enter the password';
+             }
+             if(empty($data['confirm_password'])){
+                 $data['confirm_password_err'] = 'Please confirm password';
+             }
         } else {
             $this->view('users/register');
         }
+
     }
 
     public function login() {
