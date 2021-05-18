d89126eab57759b7ecf1456fa2be80fd5868757e
Christo-IV <christo.vahtramae@khk.ee>
2021-05-18 09:11:23 +0300

    Added a colon

--- app/models/User.php
+++ app/models/User.php
@@ -13,7 +13,7 @@ class User
         $this->db = new Database();
     }
     public function findUserByEmail($email){
-        $this->db->query('SELECT * FROM users WHERE email=email');
+        $this->db->query('SELECT * FROM users WHERE email=:email');
         $this->db->bind(':email', $email);
         $user = $this->db->getOne();
         if($this->db->rowCount() > 0){
