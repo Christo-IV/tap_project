a80f6057d12372e593efef1c8fef21b10b08f15f
Sander <sander.paasalu@khk.ee>
2021-05-31 09:15:17 +0300

    Added new db log and trigger to get old contracts into log

--- app/docs/kpr.sql
+++ app/docs/kpr.sql
@@ -103,3 +103,50 @@ CREATE TABLE IF NOT EXISTS `contracts` (
 ALTER TABLE `contracts`
     ADD CONSTRAINT `contracts_ibfk_1` FOREIGN KEY (`provider_id`) REFERENCES `providers` (`id`);
 COMMIT;
+
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Loomise aeg: Mai 31, 2021 kell 08:10 EL
+-- Serveri versioon: 10.4.18-MariaDB
+-- PHP versioon: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Andmebaas: `kpr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+    `provider_id` int(10) UNSIGNED NOT NULL,
+    `customer_phone` int(14) NOT NULL,
+    `customer_email` varchar(255) NOT NULL,
+    `task` varchar(255) NOT NULL,
+    `money` decimal(15,2) NOT NULL,
+    `location` varchar(255) NOT NULL,
+    `customer_name` varchar(255) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+/* Trigger andmete contractist logi saamiseks:*/
+
+INSERT INTO log VALUES (old.provider_id, old.customer_phone, old.customer_email, old.task, old.money, old.location, old.customer_name)
